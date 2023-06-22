20f8815b4c717094da33e9d4c29468bea3e24b70
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-06-22 15:40:05 +0600

    init: initialized dot net with empty student controller
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

---
+++ Data/TestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test_dot.Data.Entities;
+
+namespace test_dot.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> context) : base(context)
+        {
+
+        }
+
+
+        public DbSet<Students> Students { get; set; }
+    }
+}
